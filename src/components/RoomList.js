133a2c00898a26382dd10532be912e0bef620d28
Jesse <jessegreyson@gmail.com>
2018-03-24 19:01:56 -0600

    finished user story and checkpoint

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -8,13 +8,25 @@ export class RoomList extends Component{
     constructor(props){
         super(props);
         this.state={
-            rooms: []
+            rooms: [],
+            newRoomName: "",
         };
 
         this.roomsRef = this.props.firebase.database().ref('rooms');
-
+        this.handleChange = this.handleChange.bind(this);
+        this.createRoom = this.createRoom.bind(this);
     }
     
+    handleChange(e) {
+        this.setState({ newRoomName: e.target.value });
+    }
+    
+    createRoom(e) {
+        e.preventDefault();
+        this.roomsRef.push({ name: this.state.newRoomName });
+        this.setState({ newRoomName: ""});
+    }
+
     componentDidMount() {
         this.roomsRef.on('child_added', snapshot => {
             const room = snapshot.val();
@@ -27,6 +39,13 @@ export class RoomList extends Component{
 
     render(){
         
+        const roomForm = (
+            <form onSubmit={this.createRoom}>
+            <input type="text" value={this.state.newRoomName} placeholder="Enter Room Name" onChange={this.handleChange}/>
+            <input type="submit" value="Create" />
+            </form>
+        );
+
         const roomList = this.state.rooms.map((room)=>
             <li key={room.key}>{room.name}</li>
         );
@@ -34,6 +53,7 @@ export class RoomList extends Component{
         return (
 
             <div>
+                <div>{roomForm}</div>
                 <ul>{roomList}</ul>
             </div>
 
